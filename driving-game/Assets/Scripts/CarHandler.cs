2f78b20e1d4c44dc3fb0f50a3812f16639125e6b
Rytis Cepulis <cepRytis@gmail.com>
2022-08-08 17:38:23 +0300

    simple moving of the car

--- driving-game/Assets/Scripts/CarHandler.cs
+++ driving-game/Assets/Scripts/CarHandler.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CarHandler : MonoBehaviour
 {
+    [SerializeField]
+    private float _power;
+    [SerializeField]
+    private float _gainSpeedPerSecond;
+    [SerializeField]
+    private float _steeringSpeed;
+    private int _steerValue = 0;
+    private float _speed;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _speed = _power * Time.deltaTime;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Acceletare();
+        Rotate();
+        Forward();
+    }
+
+    private void Acceletare()
+    {
+        _speed += _gainSpeedPerSecond * Time.deltaTime;
+    }
+
+    private void Forward()
+    {
+        transform.Translate(Vector3.forward * _speed);
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(_steeringSpeed * _steerValue * Time.deltaTime, 0f, 0f);
+    }
+
+    public void Steer(int value)
+    {
+        _steerValue = value;
     }
 }
